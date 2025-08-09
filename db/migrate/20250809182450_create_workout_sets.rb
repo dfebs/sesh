9375afcd335d76e77c8103006e9cf2c227dec8c2
dfebs <daniel.febles97@gmail.com>
2025-08-09 15:14:40 -0400

    Make reps required in WorkoutSet migration

--- db/migrate/20250809182450_create_workout_sets.rb
+++ db/migrate/20250809182450_create_workout_sets.rb
@@ -1,7 +1,7 @@
 class CreateWorkoutSets < ActiveRecord::Migration[8.0]
   def change
     create_table :workout_sets do |t|
-      t.integer :reps
+      t.integer :reps, null: false
       t.references :workout_instance, null: false, foreign_key: { on_delete: :cascade }
 
       t.timestamps
