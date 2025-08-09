fb2ee883ac46892713d9f683489334b1b020fde1
dfebs <daniel.febles97@gmail.com>
2025-08-09 17:51:11 -0400

    Add user registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  allow_unauthenticated_access only: [:new, :create]
+  def show
+    @user = Current.user
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save then
+      redirect_to new_session_path, notice: "User successfully created, time to log in!"
+    end
+  end
+
+  def new
+  end
+
+  private
+
+  def user_params
+    params.expect user: [:email_address, :password, :password_confirmation]
+  end
+end
