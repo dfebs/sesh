89efb569a98d9b107be277cc433625ff6a54724d
dfebs <daniel.febles97@gmail.com>
2025-08-10 11:46:40 -0400

    Add ability to create and look at a set of workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,26 @@
 class WorkoutsController < ApplicationController
+  def index
+    @workouts = Current.user.workouts
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    @workout.author = Current.user
+
+    if @workout.save
+      redirect_to workouts_path, notice: "Workout successfuly created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def workout_params
+    params.expect workout: [ :name, :description ]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
   has_many :workout_sessions, dependent: :destroy
-  has_many :workouts, dependent: :destroy
+  has_many :workouts, dependent: :destroy, inverse_of: "author"
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
