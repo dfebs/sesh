6cb8956059a8d28854459b23eae2050cea5c2600
dfebs <daniel.febles97@gmail.com>
2025-08-16 17:14:40 -0400

    Add ability to create and see blank workout sessions

--- app/controllers/workout_sessions_controller.rb
+++ app/controllers/workout_sessions_controller.rb
@@ -1,2 +1,26 @@
 class WorkoutSessionsController < ApplicationController
+  def index
+    @workout_sessions = Current.user.workout_sessions
+  end
+
+  def new
+    @workout_session = WorkoutSession.new
+  end
+
+  def show
+    @workout_session = WorkoutSession.find(params[:id])
+  end
+
+  def create
+    @workout_session = WorkoutSession.nw
+    @workout_session.user = Current.user
+    debugger
+    if @workout_session.save
+      redirect_to @workout_session, notice: "Session successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
 end
