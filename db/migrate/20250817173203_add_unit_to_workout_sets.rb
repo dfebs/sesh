ab566e98aab0a512763c7000caed35b64e385023
dfebs <daniel.febles97@gmail.com>
2025-08-17 13:35:34 -0400

    Make unit required in workout sets

--- db/migrate/20250817173203_add_unit_to_workout_sets.rb
+++ db/migrate/20250817173203_add_unit_to_workout_sets.rb
@@ -1,5 +1,5 @@
 class AddUnitToWorkoutSets < ActiveRecord::Migration[8.0]
   def change
-    add_column :workout_sets, :unit, :string
+    add_column :workout_sets, :unit, :string, null: false
   end
 end
