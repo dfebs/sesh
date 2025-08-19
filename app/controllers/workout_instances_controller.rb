e69c6e6293692dbf192afac8b45d00df333a9bc1
dfebs <daniel.febles97@gmail.com>
2025-08-18 21:46:58 -0400

    Allow creation of workout instances

--- app/controllers/workout_instances_controller.rb
+++ app/controllers/workout_instances_controller.rb
@@ -1,5 +1,28 @@
 class WorkoutInstancesController < ApplicationController
+  before_action :get_workout_session, only: [ :new, :create ]
+
   def new
     @workout_instance = WorkoutInstance.new
+  end
+
+  def create
+    @workout_instance = @workout_session.workout_instances.new(workout_instance_params)
+    respond_to do |format|
+      if @workout_instance.save
+        format.turbo_stream
+        format.html { redirect_to @workout_session, notice: "Workout instance created" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def get_workout_session
+    @workout_session = WorkoutSession.find(params[:workout_session_id])
+  end
+
+  def workout_instance_params
+    params.expect workout_instance: [ :workout_id ]
   end
 end
