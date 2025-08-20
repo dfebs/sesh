d9db4e37364a6dfe6e9fd3775c788bd397f9e9ab
dfebs <daniel.febles97@gmail.com>
2025-08-19 22:19:31 -0400

    Add workout sets controller

--- app/controllers/workout_sets_controller.rb
+++ app/controllers/workout_sets_controller.rb
@@ -1,2 +1,29 @@
 class WorkoutSetsController < ApplicationController
+  before_action :get_workout_instance, only: %i[new create]
+
+  # TODO see if we even need a workout instance here. Might not.
+  def new
+    @workout_set = @workout_instance.workout_sets.new
+  end
+
+  def create
+    @workout_set = @workout_instance.workout_sets.new(workout_set_params)
+    respond_to do | format |
+      if @workout_set.save
+        format.turbo_stream
+        format.html { redirect_to @workout_instance, notice: "Successfully created reps" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def workout_set_params
+    params.expect workout_set: [ :reps, :unit, :amount ]
+  end
+
+  def get_workout_instance
+    @workout_instance = WorkoutInstance.find(params[:workout_instance_id])
+  end
 end
