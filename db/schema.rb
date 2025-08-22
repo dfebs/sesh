90a4dbf2c560c011cba754bf7b125a01360992db
dfebs <daniel.febles97@gmail.com>
2025-08-21 20:27:21 -0400

    Migrate to remove units from workout sets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_21_224819) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_21_230618) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -50,7 +50,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_21_224819) do
     t.integer "workout_instance_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "unit", null: false
     t.integer "amount", null: false
     t.index ["workout_instance_id"], name: "index_workout_sets_on_workout_instance_id"
   end
