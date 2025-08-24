9d0fd35af944dd216c6d635c0fb5b205050112bd
dfebs <daniel.febles97@gmail.com>
2025-08-23 21:03:15 -0400

    Add seed for generating tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Constants::WORKOUT_TAGS.each do |title|
+  Tag.find_or_create_by!(title: title)
+end
