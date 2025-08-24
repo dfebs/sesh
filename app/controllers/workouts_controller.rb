3d59ab222f868d55613216edaa045710d9b2e3b1
dfebs <daniel.febles97@gmail.com>
2025-08-24 18:22:20 -0400

    Allow quick creation of workouts through workout templates

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -23,9 +23,46 @@ class WorkoutsController < ApplicationController
     @templates = WorkoutTemplates::WORKOUTS.select { |template| !workouts.include? template[:name] }
   end
 
+  def create_from_templates
+    template_indices = params[:template_indices].map &:to_i
+    create_workouts_with_tags_from_templates template_indices
+    redirect_to root_path
+  end
+
   private
 
   def workout_params
     params.expect workout: [ :name, :description, :workout_type ]
+  end
+
+  def create_workouts_with_tags_from_templates(indices)
+    indices.each do |index|
+      template = WorkoutTemplates::WORKOUTS[index]
+      workout = Workout.new
+      # TODO add wrap the next 2 lines in a transaction call
+
+      build_workout_from_template workout, template
+      add_tags workout, template[:tags]
+    end
+  end
+
+  def build_workout_from_template(workout, template)
+      workout.name = template[:name]
+      workout.description = template[:description]
+      workout.workout_type = template[:workout_type]
+      workout.author = Current.user
+      workout.save!
+  end
+
+  def add_tags(workout, tag_names)
+    tags = tag_names.map { |title| Tag.find_by(title: title) }
+    tags.each do |tag|
+      add_tag_registration workout, tag
+    end
+  end
+
+  def add_tag_registration(workout, tag)
+    tag_registration = TagRegistration.new(workout: workout, tag: tag)
+    tag_registration.save!
   end
 end
