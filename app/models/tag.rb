8419fde5d22818043ce4318ac572e6f827f23476
dfebs <daniel.febles97@gmail.com>
2025-08-30 15:28:20 -0400

    Show tags in workouts

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  has_many :tag_registrations, dependent: :destroy
+  has_many :workouts, through: :tag_registrations
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,6 +2,8 @@ class Workout < ApplicationRecord
   belongs_to :author, class_name: "User"
   has_many :workout_instances, dependent: :destroy
   has_many :workout_sets, through: :workout_instances
+  has_many :tag_registrations, dependent: :destroy
+  has_many :tags, through: :tag_registrations
 
   def highest_volume
     workout_instances.max_by(&:volume)&.volume || 0
