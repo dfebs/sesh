7274c25965436317e1cb4c1ea864090848321141
dfebs <daniel.febles97@gmail.com>
2025-08-30 11:08:37 -0400

    Add new constants for imperial and metric units

--- lib/constants.rb
+++ lib/constants.rb
@@ -14,6 +14,24 @@ module Constants
     "m"
   ]
 
+  UNIT_TYPES = {
+    "ft" => "imperial",
+    "lbs" => "imperial",
+    "mi" => "imperial",
+    "km" => "metric",
+    "m" => "metric",
+    "kg" => "metric"
+  }
+
+  UNIT_CONVERSION_FACTORS = {
+    "ft" => 0.3048, # Feet to meters
+    "m" => 3.2808, # Meters to feet
+    "lbs" => 0.4535, # Pounds to kilograms
+    "kg" => 2.2046, # Kilograms to pounds
+    "mi" => 1.6093, # Miles to kilometers
+    "km" => 0.6213 # Kilometers to miles
+  }
+
   WORKOUT_TYPES = [
     "Weight",
     "Long Distance",
