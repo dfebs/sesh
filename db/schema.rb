42e4174a1280359dda80f31404cf5f333aa145a5
dfebs <daniel.febles97@gmail.com>
2025-09-10 18:55:45 -0700

    Migrate to remove completed column from workout sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_28_231058) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_11_015345) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -66,7 +66,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_28_231058) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "completed", default: false, null: false
     t.index ["user_id"], name: "index_workout_sessions_on_user_id"
   end
 
