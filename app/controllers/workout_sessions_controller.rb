2f21029452c2b79b785a4a44f824d0fc5fe562bf
dfebs <daniel.febles97@gmail.com>
2025-09-10 21:21:44 -0700

    Add workout session completion

--- app/controllers/workout_sessions_controller.rb
+++ app/controllers/workout_sessions_controller.rb
@@ -1,4 +1,5 @@
 class WorkoutSessionsController < ApplicationController
+  before_action :initialize_workout_session, only: %i[show update]
   def index
     @workout_sessions = Current.user.workout_sessions
   end
@@ -8,7 +9,15 @@ class WorkoutSessionsController < ApplicationController
   end
 
   def show
-    @workout_session = WorkoutSession.find(params[:id])
+  end
+
+  def update
+    if @workout_session.ready_for_completion? && @workout_session.update(workout_session_params)
+      redirect_to workout_sessions_path, notice: "Session updated!"
+    else
+      flash.now[:alert] = "Failed to update workout session"
+      render :show, status: :unprocessable_entity
+    end
   end
 
   def create
@@ -18,9 +27,17 @@ class WorkoutSessionsController < ApplicationController
     if @workout_session.save
       redirect_to @workout_session, notice: "Session successfully created"
     else
+      flash.now[:alert] = "Failed to create workout session"
       render :new, status: :unprocessable_entity
     end
   end
 
   private
+  def initialize_workout_session
+    @workout_session = WorkoutSession.find(params[:id])
+  end
+
+  def workout_session_params
+    params.expect workout_session: [ :date_completed ]
+  end
 end
--- app/models/workout_session.rb
+++ app/models/workout_session.rb
@@ -1,4 +1,22 @@
 class WorkoutSession < ApplicationRecord
   belongs_to :user
   has_many :workout_instances, dependent: :destroy
+
+  def completed?
+    date_completed.present?
+  end
+
+  def ready_for_completion?
+    if workout_instances.length == 0
+      return false
+    end
+
+    workout_instances.each do |workout_instance|
+      if workout_instance.workout_sets.length == 0
+        return false
+      end
+    end
+
+    true
+  end
 end
