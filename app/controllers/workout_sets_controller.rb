d75a8806987eb6dec6afeb9534ffa29d1ebe97b6
dfebs <daniel.febles97@gmail.com>
2025-09-17 20:47:06 -0400

    Add ability to edit workout sets

--- app/controllers/workout_sets_controller.rb
+++ app/controllers/workout_sets_controller.rb
@@ -1,13 +1,12 @@
 class WorkoutSetsController < ApplicationController
   before_action :get_workout_instance, only: %i[new create]
+  before_action :get_workout_set, only: %i[destroy edit update]
 
-  # TODO see if we even need a workout instance here. Might not.
   def new
     @workout_set = @workout_instance.workout_sets.new
   end
 
   def destroy
-    @workout_set = WorkoutSet.find(params[:id])
     @workout_session = @workout_set.workout_session
     @workout_set.destroy!
 
@@ -29,6 +28,21 @@ class WorkoutSetsController < ApplicationController
     end
   end
 
+  def edit
+    @workout_instance = @workout_set.workout_instance
+  end
+
+  def update
+    respond_to do | format |
+      if @workout_set.update(workout_set_params)
+        format.turbo_stream
+        format.html { redirect_to @workout_set.workout_session, notice: "Successfully updated reps" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
   def workout_set_params
     params.expect workout_set: [ :reps, :amount_imp, :amount_metric ]
@@ -36,5 +50,9 @@ class WorkoutSetsController < ApplicationController
 
   def get_workout_instance
     @workout_instance = WorkoutInstance.find(params[:workout_instance_id])
+  end
+
+  def get_workout_set
+    @workout_set = WorkoutSet.find(params[:id])
   end
 end
