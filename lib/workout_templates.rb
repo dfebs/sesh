f5808fc677617d8ad2e8655cc8a0e5af16041231
dfebs <daniel.febles97@gmail.com>
2025-09-21 21:45:51 -0400

    Add more workout templates

--- lib/workout_templates.rb
+++ lib/workout_templates.rb
@@ -12,6 +12,28 @@ module WorkoutTemplates
       ]
     },
     {
+      name: "Incline Push-Ups",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Chest",
+        "Shoulders",
+        "Triceps",
+        "Core"
+      ]
+    },
+    {
+      name: "Decline Push-Ups",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Chest",
+        "Shoulders",
+        "Triceps",
+        "Core"
+      ]
+    },
+    {
       name: "Russian Twists",
       workout_type: "Weight",
       description: "--",
@@ -20,7 +42,23 @@ module WorkoutTemplates
       ]
     },
     {
-      name: "Barbell Squats",
+      name: "Lunges",
+      workout_type: "Weight",
+      description: "--",
+      tags: [
+        "Legs"
+      ]
+    },
+    {
+      name: "Lateral Lunges",
+      workout_type: "Weight",
+      description: "--",
+      tags: [
+        "Legs"
+      ]
+    },
+    {
+      name: "Squats",
       workout_type: "Weight",
       description: "--",
       tags: [
@@ -38,12 +76,163 @@ module WorkoutTemplates
       ]
     },
     {
+      name: "Neutral Pull-Ups",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Upper Back",
+        "Core",
+        "Biceps"
+      ]
+    },
+    {
+      name: "Chin-Ups",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Upper Back",
+        "Core",
+        "Biceps",
+      ]
+    },
+    {
       name: "Outdoor Run",
       description: "--",
       workout_type: "Long Distance",
       tags: [
         "Legs"
       ]
-    }
+    },
+    {
+      name: "Indoor Run",
+      description: "--",
+      workout_type: "Long Distance",
+      tags: [
+        "Legs"
+      ]
+    },
+    {
+      name: "Incline Dumbbell Curl",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Biceps"
+      ]
+    },
+    {
+      name: "Inverse Rows",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Biceps",
+        "Upper Back",
+        "Lower Back",
+        "Shoulders"
+      ]
+    },
+    {
+      name: "V-Ups",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Core"
+      ]
+    },
+    {
+      name: "Alternating V-Ups",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Core"
+      ]
+    },
+    {
+      name: "Bulgarian Split Squat",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Legs"
+      ]
+    },
+    {
+      name: "Romanian Deadlift",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Core",
+        "Legs",
+        "Lower Back"
+      ]
+    },
+    {
+      name: "In and Outs",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Core"
+      ]
+    },
+    {
+      name: "Skull Crushers",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Triceps"
+      ]
+    },
+    {
+      name: "Cable Wood Chops",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Core"
+      ]
+    },
+    {
+      name: "Bench Press",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Chest",
+        "Triceps",
+        "Shoulders"
+      ]
+    },
+    {
+      name: "Bosu Mountain Climbers",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Core"
+      ]
+    },
+    {
+      name: "Hanging Leg Raises",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Core",
+        "Legs"
+      ]
+    },
+    {
+      name: "Step-Ups",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Legs"
+      ]
+    },
+    {
+      name: "Dips",
+      description: "--",
+      workout_type: "Weight",
+      tags: [
+        "Chest",
+        "Shoulders",
+        "Core",
+        "Triceps"
+      ]
+    },
   ]
 end
