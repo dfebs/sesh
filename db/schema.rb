a816ff95603cdbd7309cdfde5205bee77fbd05cd
dfebs <daniel.febles97@gmail.com>
2025-10-11 17:55:27 -0400

    Perform migration for adding email_confirmed to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_20_133942) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_11_195635) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -50,6 +50,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_20_133942) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "email_confirmed", default: false, null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
