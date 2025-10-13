e381535fd82e376b55c4d1db92d35bb0885b13ca
dfebs <daniel.febles97@gmail.com>
2025-10-13 19:17:29 -0400

    Modify passwords mailer to be more compatible with Postmark

--- app/mailers/passwords_mailer.rb
+++ app/mailers/passwords_mailer.rb
@@ -1,6 +1,8 @@
 class PasswordsMailer < ApplicationMailer
+  default from: "hello@dfebs.com"
+
   def reset(user)
     @user = user
-    mail subject: "Reset your password", to: user.email_address
+    mail to: user.email_address, subject: "Reset your password", track_opens: "true", message_stream: "outbound"
   end
 end
