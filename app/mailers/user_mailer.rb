30d37a69ec822fed96851d214750b4e3e3969188
dfebs <daniel.febles97@gmail.com>
2025-10-13 19:17:08 -0400

    Modify user mailer to include a subject

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
 
   def email_confirmation
     @user = params[:user]
-    mail to: @user.email_address, from: "hello@dfebs.com", track_opens: "true", message_stream: "outbound"
+    mail to: @user.email_address, from: "hello@dfebs.com", subject: "Verify your email with Sesh", track_opens: "true", message_stream: "outbound"
   end
 end
