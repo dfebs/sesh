5a7d26faedb106ed3369c24509495378ea831b6f
dfebs <daniel.febles97@gmail.com>
2025-10-25 17:24:46 -0400

    Go directly to relevant workout when linking from workout table

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "chart.js"
+
+document.addEventListener('turbo:load', function() {
+    if (window.location.hash) {
+        window.location.hash = window.location.hash;
+    }
+});
