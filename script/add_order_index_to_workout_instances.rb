f6b618b14224194e2d25ee6d02c362dcc767b3ce
dfebs <daniel.febles97@gmail.com>
2025-11-04 17:43:16 -0500

    Update script to save workout instance after setting order index

--- script/add_order_index_to_workout_instances.rb
+++ script/add_order_index_to_workout_instances.rb
@@ -5,6 +5,7 @@ workout_sessions.each do  | workout_session |
   current_index = 1.0
   workout_session.workout_instances.each do | workout_instance |
     workout_instance.order_index = current_index
+    workout_instance.save!
     current_index += 1.0
   end
 end
