a8e8148ac5d16d1ecbb0759dd801522a95d2333b
dfebs <daniel.febles97@gmail.com>
2025-11-07 08:19:00 -0500

    Add time zone constants

--- lib/constants.rb
+++ lib/constants.rb
@@ -50,4 +50,39 @@ module Constants
     "Cardio",
     "Stretch"
   ]
+
+  TIME_ZONES = [
+    "Pacific/Midway",            # UTC-11
+    "America/Adak",              # UTC-10, Alaska
+    "Pacific/Honolulu",          # UTC-10, Hawaii
+    "America/Anchorage",         # UTC-9
+    "America/Los_Angeles",       # UTC-8, Pacific Time
+    "America/Denver",            # UTC-7, Mountain Time
+    "America/Chicago",           # UTC-6, Central Time
+    "America/New_York",          # UTC-5, Eastern Time
+    "America/Caracas",           # UTC-4:30
+    "America/Santiago",          # UTC-4, Chile
+    "America/Halifax",           # UTC-4, Atlantic Time (Canada)
+    "America/St_Johns",          # UTC-3:30, Newfoundland
+    "America/Argentina/Buenos_Aires", # UTC-3
+    "America/Sao_Paulo",         # UTC-3, Brazil
+    "Atlantic/Azores",           # UTC-1
+    "Europe/London",             # UTC+0, UK
+    "Europe/Paris",              # UTC+1, Central Europe
+    "Europe/Athens",             # UTC+2, Eastern Europe
+    "Africa/Cairo",              # UTC+2, Egypt
+    "Europe/Moscow",             # UTC+3
+    "Asia/Dubai",                # UTC+4, UAE
+    "Asia/Karachi",              # UTC+5, Pakistan
+    "Asia/Kolkata",              # UTC+5:30, India
+    "Asia/Dhaka",                # UTC+6, Bangladesh
+    "Asia/Jakarta",              # UTC+7, Indonesia
+    "Asia/Shanghai",             # UTC+8, China
+    "Asia/Tokyo",                # UTC+9, Japan
+    "Australia/Adelaide",        # UTC+9:30
+    "Australia/Sydney",          # UTC+10
+    "Pacific/Noumea",            # UTC+11, New Caledonia
+    "Pacific/Auckland",          # UTC+12, New Zealand
+    "Pacific/Kiritimati"         # UTC+14, Line Islands (earliest time zone)
+  ]
 end
