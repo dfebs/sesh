4834d5f1fae28a5a9246d05909310c4a997cca2a
dfebs <daniel.febles97@gmail.com>
2025-11-16 16:32:26 -0500

    Migrate to add time zone to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_25_164436) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_16_212621) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -51,6 +51,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_25_164436) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "email_confirmed", default: false, null: false
+    t.string "time_zone", default: "America/New York", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
