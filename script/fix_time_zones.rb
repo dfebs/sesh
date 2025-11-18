de6c9890e6362c9d9ad85b7795adc50ec3e0fe6a
dfebs <daniel.febles97@gmail.com>
2025-11-17 19:00:31 -0500

    Fix script for fixing time zones

--- script/fix_time_zones.rb
+++ script/fix_time_zones.rb
@@ -1,6 +1,5 @@
 
 require_relative "../config/environment"
 
-users = Users.where(time_zone: "America/New York");
+users = User.where(time_zone: "America/New York")
 users.update_all(time_zone: "America/New_York")
-users.save!
